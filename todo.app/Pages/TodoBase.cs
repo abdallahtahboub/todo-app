d81efa48f278a84f86aa41cc411c95e666833848
abdallahtahboub <tahboub252@gmail.com>
2022-02-10 15:29:46 +0100

    Assigning Ids to the Items

--- todo.app/Pages/TodoBase.cs
+++ todo.app/Pages/TodoBase.cs
@@ -12,11 +12,17 @@ namespace todo.app
     public class ToDoBase : ComponentBase
     {
         public Item _Item;
+        public int _Counter = 0;
         public List<Item> _Items = new List<Item>();
 
         public List<Item> AddItem(string item)
         {
+            
+
+            
             _Item = new Item();
+            _Item.ItemId = _Counter++;
+            Console.WriteLine(_Counter);
             _Item.Value = item;
             _Items.Add(_Item);
 
