23fe488d1b936a2a275c7bd1cb9f89d5b77a9723
abdallahtahboub <tahboub252@gmail.com>
2022-07-08 11:29:24 +0200

    Adding User Entity

---
+++ todo.data/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace todo.business
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public ICollection<Item> Items { get; set; }
+    }
+}
