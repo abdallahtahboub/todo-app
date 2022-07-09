259f0aa69d66a41e07fb176777b261b0ba9d3c2f
abdallahtahboub <tahboub252@gmail.com>
2022-07-09 17:32:25 +0200

    adding Configuration for the database connection in the startup class

--- todo.api/Startup.cs
+++ todo.api/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using todo.data;
 using Microsoft.EntityFrameworkCore;
@@ -21,22 +15,24 @@ namespace todo.api
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // configuring and registering connectionString to sqlserver
+            services.AddDbContextPool<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Configuring and registering the SQL repository (saving and reteeieving from database).
+            // using addscoped method in order for the instance sql repository class to be alive and available of the entire scope of an http request.  
+            services.AddScoped<IToDoRepository, SQLToDoRepository>();
+            //  Configuring and registering the SQL repository (saving and reteeieving from InMemory collection)  
+            services.AddTransient<IToDoRepository, TodoRepositoryInMemory>();
 
-            // configuring connectionString to sqlserver
-            services.AddDbContextPool<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ToDoConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "todo.api", Version = "v1" });
             });
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
