6ad83b115961e70bb28b63005f422a2740fa568e
Abdallah Tahboub <30658046+abdallahtahboub@users.noreply.github.com>
2023-08-08 09:39:27 +0200

    Update ToDo.cs
    removing empty spaces

--- todo.business/ToDo.cs
+++ todo.business/ToDo.cs
@@ -19,42 +19,28 @@ namespace todo.business
                 _Item.ItemId = _Counter++;
                 _Item.Value = item;
                 _Items.Add(_Item);
-
-
             }
-
             return _Item;
-
         }
 
         public void DeleteItem(int id)
         {
-
-
             foreach (Item item in _Items)
             {
-
                 if (item.ItemId == id)
                 {
-
                     _Items.Remove(item);
                     break;
                 }
-
-
             }
-
-
-
         }
+        
         // To generate Unique ids to help identifying list items and elements whitch helps with test automation.
         public int GenerateUUIDs()
         {
-
             Random randomUuid = new Random();
             int generatedUuid = randomUuid.Next();
             return generatedUuid;
-
         }
 
     }
