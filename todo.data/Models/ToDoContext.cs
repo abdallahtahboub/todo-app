aeab74b274cc7bd5ccef47594be5b0d261bcdf57
Abdallah Tahboub <tahboub252@gmail.com>
2025-02-14 16:17:39 +0200

    cleaning up prjects

--- todo.data/Models/ToDoContext.cs
+++ todo.data/Models/ToDoContext.cs
@@ -3,14 +3,14 @@ using Microsoft.EntityFrameworkCore;
 namespace todo.data
 {
     public class ToDoContext : DbContext
-{
-    public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
-
-    public DbSet<Item> Items { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
     }
-}
 }
