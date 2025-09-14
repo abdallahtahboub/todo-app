be2993656f6537c172f43a0b923647c028c193b8
Abdallah Tahboub <atahboub94@outlook.com>
2025-09-14 17:05:20 +0200

    Updating business logic (interface and its  implementation) (#10)

--- todo.business/Services/IToDoService.cs
+++ todo.business/Services/IToDoService.cs
@@ -4,6 +4,7 @@ public interface IToDoService
 
     public Item AddItem(string item);
     public Item GetItem(int id);
+    public IEnumerable<Item> GetAll();
     public Item UpdateItem(int id, string newValue);
     public Item MarkAsCompleted(int id);
     public bool DeleteItem(int id);
